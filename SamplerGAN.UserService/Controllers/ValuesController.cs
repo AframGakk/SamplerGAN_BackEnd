4215ab4c2f435e7d1bc287fba95beed134fad409
IvarKristinn <hallssonivarkristinn@gmail.com>
2019-09-26 16:23:58 +0000

    userservice init

---
+++ SamplerGAN.UserService/Controllers/ValuesController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SamplerGAN.UserService.Controllers
+{
+    [Route("api/users")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        // GET api/users
+        [Route("")]
+        [HttpGet]
+        public ActionResult<IEnumerable<string>> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/values/5
+        [Route("{id:int}")]
+        [HttpGet]
+        public ActionResult<string> Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/values
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+            return;
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+            return;
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+            return;
+        }
+    }
+}
