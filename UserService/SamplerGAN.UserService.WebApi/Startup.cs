a338700baf3733785f47538d33ff0eeb93b3fbc9
IvarKristinn <hallssonivarkristinn@gmail.com>
2019-12-06 12:09:24 +0000

    CORS

--- UserService/SamplerGAN.UserService.WebApi/Startup.cs
+++ UserService/SamplerGAN.UserService.WebApi/Startup.cs
@@ -20,46 +20,51 @@ using SamplerGAN.UserService.WebApi.ExceptionHandlerExtensions;
 
 namespace SamplerGAN.UserService.WebApi
 {
-    public class Startup
+  public class Startup
+  {
+    public Startup(IConfiguration configuration)
     {
-        public Startup(IConfiguration configuration)
-        {
-            Configuration = configuration;
-        }
-
-        public IConfiguration Configuration { get; }
-
-        // This method gets called by the runtime. Use this method to add services to the container.
-        public void ConfigureServices(IServiceCollection services)
-        {
-            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
-            services.AddDbContext<UserContext>(opt =>
-                opt.UseNpgsql(Configuration.GetConnectionString("DefaultConnection")));
-            services.AddTransient<IUserServices, UserServices>();
-            services.AddTransient<IUserRepository, UserRepository>();
-
-            services.Configure<ApiBehaviorOptions>(opt => 
-            {
-                opt.SuppressModelStateInvalidFilter = true;
-            });
-        }
-
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
-        {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
-            else
-            {
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-                app.UseHsts();
-            }
-
-            app.UseHttpsRedirection();
-            app.UseGlobalExceptionHandler();
-            app.UseMvc();
-        }
+      Configuration = configuration;
     }
+
+    public IConfiguration Configuration { get; }
+
+    // This method gets called by the runtime. Use this method to add services to the container.
+    public void ConfigureServices(IServiceCollection services)
+    {
+      services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+      services.AddDbContext<UserContext>(opt =>
+          opt.UseNpgsql(Configuration.GetConnectionString("DefaultConnection")));
+      services.AddTransient<IUserServices, UserServices>();
+      services.AddTransient<IUserRepository, UserRepository>();
+
+      services.Configure<ApiBehaviorOptions>(opt =>
+      {
+        opt.SuppressModelStateInvalidFilter = true;
+      });
+    }
+
+    // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+    public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+    {
+      /*if (env.IsDevelopment())
+      {
+          app.UseDeveloperExceptionPage();
+      }
+      else
+      {
+          // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+          app.UseHsts();
+      }
+*/
+      // global cors policy
+      app.UseCors(x => x
+          .AllowAnyOrigin()
+          .AllowAnyMethod()
+          .AllowAnyHeader());
+      app.UseHttpsRedirection();
+      app.UseGlobalExceptionHandler();
+      app.UseMvc();
+    }
+  }
 }
